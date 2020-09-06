70498f99aa3cdf5a3eaed0008a232e5a1e3f4959
aalcamo <aalcamo@gofurthercorp.com>
2020-09-05 20:25:02 -0400

    added spring security.

---
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package guru.sfg.brewery.eureka.msscbreweryeureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * using spring security, we are disabling csrf and all requests need to be
+     * authenticated using basic http authentication. the user and password are
+     * currently defined in application.properties.
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
